cf6e3afb3080b1850ddb3bd4d5dfe96ff0d4540b
s-tyda <styda@huhha.co>
2021-03-20 14:17:39 +0100

    Updated forward chainings

--- SebTyd_forward_chaining_negation.py
+++ SebTyd_forward_chaining_negation.py
@@ -1,39 +1,34 @@
 # Open file and write it to the list of stripped lines
-data = [x.strip() for x in open("resources/baza-wiedzy-negacja.txt").readlines()]
+data = [x.strip() for x in open("resources/baza-wiedzy-negacja-2.txt").readlines()]
 
 # Add facts to the list
 facts = data[-1].split(",")
 
-# Add formulas to the list
-formulas = []
+# Add formulas to the dictionary, where conclusions are keys
+formulas = {}
 for formula in data[:-2]:
-    splitted_formula = formula.split("->")
+    splitted_formula = formula[2:].split("->")
     conclusion = splitted_formula[1]
-    splitted_formula = splitted_formula[0].split(".")
-    conditions = splitted_formula[1].split(",")
-    formulas.append((conditions, conclusion))
+    conditions = splitted_formula[0].split(",")
+    if conclusion not in formulas:
+        formulas.update({conclusion: []})
+    formulas[conclusion].append(conditions)
+
+
+def check_formula(formula):
+    # Function to check a single formula
+    return all(x[1] not in facts if x.startswith("~") else x[0] in facts for x in formula)
 
 
 # Make forward chaining
 is_changed = True
 while is_changed:
     is_changed = False
-    for formula in formulas:
-        conditions, fact = formula
-        check = True
-        for condition in conditions:
-            if condition.startswith("~"):
-                condition = condition[1:]
-                if condition in facts:
-                    check = False
-                    if fact in facts:
-                        facts.remove(fact)
-                        is_changed = True
-            else:
-                if condition not in facts:
-                    check = False
-
-        if check and fact not in facts:
+    for fact in formulas:
+        if fact in facts and not any(check_formula(x) for x in formulas[fact]):
+            facts.remove(fact)
+            is_changed = True
+        elif fact not in facts and any(check_formula(x) for x in formulas[fact]):
             facts.append(fact)
             is_changed = True
 
--- SebTyd_forward_chaining_v2.py
+++ SebTyd_forward_chaining_v2.py
@@ -5,21 +5,28 @@ data = [x.strip() for x in open("resources/Wnioski.txt").readlines()]
 # Add facts to the list
 facts = data[-1].split(",")
 
-# Add formulas to the list
-formulas = []
+# Add formulas to the dictionary, where conclusions are keys
+formulas = {}
 for formula in data[:-2]:
     splitted_formula = formula.split()
     conditions = splitted_formula[1].split(",")
     conclusion = splitted_formula[3]
-    formulas.append((conditions, conclusion))
+    if conclusion not in formulas:
+        formulas.update({conclusion: []})
+    formulas[conclusion].append(conditions)
+
+
+def check_formula(formula):
+    # Function to check a single formula
+    return all(x in facts for x in formula)
+
 
 # Make forward chaining
 is_changed = True
 while is_changed:
     is_changed = False
-    for formula in formulas:
-        conditions, fact = formula
-        if fact not in facts and all(x in facts for x in conditions):
+    for fact in formulas:
+        if fact not in facts and any(check_formula(formula) for formula in formulas[fact]):
             facts.append(fact)
             is_changed = True
 
